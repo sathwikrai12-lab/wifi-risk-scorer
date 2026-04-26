3baa349b2cdb8ec68b5fa03124336b48d96bd682
Sathwik Rai <sathwikrai12@gmail.com>
2026-04-26 10:17:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,8 @@ def score():
         level = "Dangerous"
 
     return jsonify({"risk": risk, "level": level})
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
